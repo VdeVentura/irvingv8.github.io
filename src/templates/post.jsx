bd00e170dc2b711918c60d63f631979e157470fe
Irving Ventura <irvingv8@gmail.com>
2017-08-07 01:29:07 -0400

    -> Posts Layout

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Helmet from "react-helmet";
-import UserInfo from "../components/UserInfo/UserInfo";
 import Disqus from "../components/Disqus/Disqus";
 import PostTags from "../components/PostTags/PostTags";
 import SocialLinks from "../components/SocialLinks/SocialLinks";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 import "./b16-tomorrow-dark.css";
-import "./post.css";
+import "./post.scss";
 
 export default class PostTemplate extends React.Component {
   render() {
@@ -26,16 +25,17 @@ export default class PostTemplate extends React.Component {
           <title>{`${post.title} | ${config.siteTitle}`}</title>
         </Helmet>
         <SEO postPath={slug} postNode={postNode} postSEO />
-        <div>
-          <h1>
-            {post.title}
-          </h1>
+        <div className="full-post">
+          <div className="title">
+            <h1>
+              {post.title}
+            </h1>
+          </div>
           <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
           <div className="post-meta">
             <PostTags tags={post.tags} />
             <SocialLinks postPath={slug} postNode={postNode} />
           </div>
-          <UserInfo config={config} />
           <Disqus post={post} />
         </div>
       </div>
