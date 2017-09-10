0e75911c83df2c048a99191703532e5eeeb14c08
Irving Ventura <irvingv8@gmail.com>
2017-09-10 16:26:04 -0400

    -> adding proper urls to social networks

--- src/components/SiteLinks/index.jsx
+++ src/components/SiteLinks/index.jsx
@@ -7,34 +7,34 @@ class SiteLinks extends React.Component {
       <div className="blog-links">
         <ul>
             <li>
-              <a href="#">
+              <a href="https://www.instagram.com/irvingv8/">
                 <i className="fa fa-instagram" />
               </a>
             </li>
             <li>
-              <a href="#">
+              <a href="https://www.facebook.com/irvingv8">
                 <i className="fa fa-facebook" />
               </a>
             </li>
             <li>
-              <a href="#">
+              <a href="https://twitter.com/irvingv8">
                 <i className="fa fa-twitter" />
               </a>
             </li>
         </ul>
         <ul>
             <li>
-              <a href="#">
+              <a href="https://github.com/irvingv8">
                 <i className="fa fa-github" />
               </a>
             </li>
             <li>
-              <a href="#">
+              <a href="https://codepen.io/irvingv8/">
                 <i className="fa fa-codepen" />
               </a>
             </li>
             <li>
-              <a href="#">
+              <a href="mailto:irvingv8@gmail.com">
                 <i className="fa fa-envelope-o" />
               </a>
             </li>
