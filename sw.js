e93cfb070312ace625400836d3981e9fa4da8e81
Irving Ventura <irvingv8@gmail.com>
2017-09-10 16:43:23 -0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/gatsby-advanced-starter/app-e50955437454ef0c2ea0.js","bb768921e5af151969a49940719e8beb"],["/gatsby-advanced-starter/commons-e27db60033abd0082485.js","8b9b556d3e017e1ed90172be65a7db7f"],["/gatsby-advanced-starter/index.html","fc5359e2badbb3b7ecee5d1342b7c79c"],["/gatsby-advanced-starter/manifest.json","d6bbed8240c40b56b65d3fda4e4faf07"],["/gatsby-advanced-starter/offline-plugin-app-shell-fallback/index.html","c7a3aafa30dfc0f20f8e5a04acc56008"]];
+var precacheConfig = [["/app-c3e44ccb2599740a8125.js","709565791f3bec41290be8ac1b844ed4"],["/commons-e27db60033abd0082485.js","53f116cb591580d84936cfbede657ba3"],["/index.html","7734d5a77c430eac4aa72fc0b4a1a7cf"],["/manifest.json","d6bbed8240c40b56b65d3fda4e4faf07"],["/offline-plugin-app-shell-fallback/index.html","fcad0bd72bcd295d50f36e300d7f9498"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
