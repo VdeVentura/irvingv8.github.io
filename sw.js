e35f3c0abef151e9eb56bd015995021023b7de13
Irving Ventura <irvingv8@gmail.com>
2017-11-09 11:57:57 -0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-eec407baa6cbffa1afee.js","7ee8f10c8887696ca1be95743952f7db"],["/commons-e27db60033abd0082485.js","53f116cb591580d84936cfbede657ba3"],["/index.html","35ee5448c7cd15538b32d299d6307d06"],["/manifest.json","d6bbed8240c40b56b65d3fda4e4faf07"],["/offline-plugin-app-shell-fallback/index.html","b86af19c3da9c7759884eed005d515cd"]];
+var precacheConfig = [["/app-156c5ec56fdd61559c31.js","1ed6a8979498da8705b52e693fd7cdbe"],["/commons-e27db60033abd0082485.js","53f116cb591580d84936cfbede657ba3"],["/index.html","e486e9e1ee7ece9f765aace66bb2ad6c"],["/manifest.json","d6bbed8240c40b56b65d3fda4e4faf07"],["/offline-plugin-app-shell-fallback/index.html","05440e5fa206ade309f8e2ec187a2b3b"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
